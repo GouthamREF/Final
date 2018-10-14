37dc7ae16fb9a3330459108fcef59b58c5d3f434
NAAASTIT <nasreentit@gmail.com>
2018-10-14 19:06:44 +0530

    Committttt

--- src/businesslib/Scenerio_busi.java
+++ src/businesslib/Scenerio_busi.java
@@ -8,9 +8,9 @@ public class Scenerio_busi
 	public static void Scenerio1()
 	{
 		Driver.dr.findElement(Scenerio.electronics).click();
-		Driver.dr.findElement(Scenerio.laptop).click();
-		Driver.dr.findElement(Scenerio.mac).click();
-		Driver.dr.findElement(Scenerio.order).click();
+		//Driver.dr.findElement(Scenerio.laptop).click();
+		//Driver.dr.findElement(Scenerio.mac).click();
+		//Driver.dr.findElement(Scenerio.order).click();
 	}
 
 
